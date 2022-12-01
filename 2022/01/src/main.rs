ff79c83b72155f4d7762d9e380d6813b9e75f3d2
Tomas Vybiral <vybiraltomas@seznam.cz>
2022-12-01 19:46:34 +0100

    Completed 01-02-2022

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -1,16 +1,16 @@
 use std::fs::File;
-use std::io::{BufReader, prelude::*};
+use std::io::{prelude::*, BufReader};
 
 struct CaloriesCounter {
     sum: u64,
-    max_cal: u64
+    max_cal: [u64; 3],
 }
 
 impl CaloriesCounter {
     fn new() -> Self {
         Self {
             sum: 0,
-            max_cal: 0
+            max_cal: [0, 0, 0],
         }
     }
 
@@ -18,15 +18,38 @@ impl CaloriesCounter {
         match line.parse::<u64>() {
             Ok(calories) => {
                 self.sum += calories;
-            },
+            }
             Err(_e) => {
-                if self.sum > self.max_cal {
-                    self.max_cal = self.sum;
-                }
-                self.sum = 0;
+                self.process_sum();
             }
         }
     }
+
+    fn process_sum(&mut self) {
+        if self.sum > self.max_cal[0] {
+            self.max_cal[2] = self.max_cal[1];
+            self.max_cal[1] = self.max_cal[0];
+            self.max_cal[0] = self.sum;
+        } else if self.sum > self.max_cal[1] {
+            self.max_cal[2] = self.max_cal[1];
+            self.max_cal[1] = self.sum;
+        } else if self.sum > self.max_cal[2] {
+            self.max_cal[2] = self.sum;
+        }
+        println!(
+            "{} => {}, {}, {}",
+            self.sum, self.max_cal[0], self.max_cal[1], self.max_cal[2]
+        );
+        self.sum = 0;
+    }
+}
+
+fn sum(numbers: &[u64]) -> u64 {
+    let mut sum = 0 as u64;
+    for number in numbers.iter() {
+        sum += number;
+    }
+    sum
 }
 
 fn main() {
@@ -36,8 +59,9 @@ fn main() {
     for line_res in reader.lines() {
         match line_res {
             Ok(line) => counter.process_line(&line),
-            Err(_e) => break
+            Err(_e) => break,
         }
     }
-    println!("{}", counter.max_cal);
+    counter.process_sum();
+    println!("{}", sum(&counter.max_cal));
 }
