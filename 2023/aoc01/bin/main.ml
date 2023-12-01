259b5c69194b831eb6997e8f7b169968c92dae44
Tomas Vybiral <vybiraltomas@seznam.cz>
2023-12-01 21:17:14 +0100

    Not solved 23/01-2

--- 2023/aoc01/bin/main.ml
+++ 2023/aoc01/bin/main.ml
@@ -7,8 +7,23 @@ let rec explode text =
     | non_empty -> non_empty.[0] :: explode (String.sub non_empty 1 (String.length non_empty - 1))
 
 let rec process_line line first last =
+    let helper rest first peek =
+        if first = none_number then
+            process_line rest peek peek
+        else
+            process_line rest first peek
+    in
     match line with
     | [] -> int_of_string (Printf.sprintf "%c%c" first last)
+    | 'o' :: 'n' :: 'e' :: rest -> helper rest first '1'
+    | 't' :: 'w' :: 'o' :: rest -> helper rest first '2'
+    | 't' :: 'h' :: 'r' :: 'e' :: 'e' :: rest -> helper rest first '3'
+    | 'f' :: 'o' :: 'u' :: 'r' :: rest -> helper rest first '4'
+    | 'f' :: 'i' :: 'v' :: 'e' :: rest -> helper rest first '5'
+    | 's' :: 'i' :: 'x' :: rest -> helper rest first '6'
+    | 's' :: 'e' :: 'v' :: 'e' :: 'n' :: rest -> helper rest first '7'
+    | 'e' :: 'i' :: 'g' :: 'h' :: 't' :: rest -> helper rest first '8'
+    | 'n' :: 'i' :: 'n' :: 'e' :: rest -> helper rest first '9'
     | c :: rest -> match c with
         | '0'
         | '1'
@@ -19,20 +34,18 @@ let rec process_line line first last =
         | '6'
         | '7'
         | '8'
-        | '9' -> if first = none_number then
-                process_line rest c c
-            else
-                process_line rest first c
+        | '9' -> helper rest first c 
         | _ -> process_line rest first last
 
 let rec process_file file =
     let line = try input_line file with End_of_file -> "" in
     match String.length line with
     | 0 -> 0
-    | _ -> process_line (explode line) none_number none_number + process_file file
+    | _ -> 
+        let line_res = process_line (explode line) none_number none_number in
+        line_res + process_file file
 
 let () =
     let file = open_in file_name in
     let res = process_file file in
-    print_int res
-    
+    Printf.printf "%i\n" res
