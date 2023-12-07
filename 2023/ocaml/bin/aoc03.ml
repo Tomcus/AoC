0dcaaab9b8a70e338b0878589a33a75c68f1df06
Tomas Vybiral <vybiraltomas@seznam.cz>
2023-12-07 21:28:34 +0100

    Solved 23/03/02

--- 2023/ocaml/bin/aoc03.ml
+++ 2023/ocaml/bin/aoc03.ml
@@ -20,42 +20,52 @@ let rec parse_line_impl line read numbers chars =
             let new_nums = (num, read, read + num_len - 1) :: numbers in
             parse_line_impl rest (read + num_len) new_nums chars
         )
-        | '.' -> parse_line_impl (String.sub line 1 (line_len - 1)) (read + 1) numbers chars
-        | _ ->
-            let new_chars = read :: chars in
+        | '*' ->
+            let new_chars = (read, -1, -1) :: chars in
             parse_line_impl (String.sub line 1 (line_len - 1)) (read + 1) numbers new_chars
+        | _ -> parse_line_impl (String.sub line 1 (line_len - 1)) (read + 1) numbers chars
             
 
 let parse_line line =
     parse_line_impl line 0 [] []
 
-let rec find_collision chars num_start num_end =
+let rec sum_gear_ratio chars num num_start num_end =
     match chars with
-    | [] -> false
-    | pos :: rest ->
+    | [] -> (0, chars)
+    | (pos, first, second) :: rest ->
         if pos >= (num_start - 1) && pos <= (num_end + 1) then
-            true
+            if first = -1 then (
+                (0, (pos, num, -1) :: rest)
+            ) else (
+                (first * num, rest)
+            )
         else
-            find_collision rest num_start num_end
+            match sum_gear_ratio rest num num_start num_end with
+            | (res, new_chars) -> 
+                (res, (pos, first, second) :: new_chars)
+
 
 let rec process_collisions current_sum nums chars =
     match nums with
+    | [] -> (current_sum, nums, chars)
     | (num, num_start, num_end) :: rest ->
-        if find_collision chars num_start num_end then
-            process_collisions (current_sum + num) rest chars
-        else
-            (match process_collisions current_sum rest chars with
-            | (sum, new_nums) ->
-                (sum, (num, num_start, num_end) :: new_nums))
-    | [] -> (current_sum, nums)
+        (match sum_gear_ratio chars num num_start num_end with
+        | (gear_ration, new_chars) ->
+            if gear_ration > 0 then
+                process_collisions (gear_ration + current_sum) rest new_chars
+            else
+                match process_collisions current_sum rest new_chars with
+                | (sum, new_nums, new_chars) ->
+                    (sum, (num, num_start, num_end) :: new_nums, new_chars)
+        )
 
 let process_data nums prev_nums chars prev_chars =
     match process_collisions 0 prev_nums chars with
-    | (sum1, _rem) ->
+    | (sum1, _rem, chars) ->
         match process_collisions sum1 nums prev_chars with
-        | (sum2, nums1) ->
+        | (sum2, nums1, _prev_chars) ->
             match process_collisions sum2 nums1 chars with
-            | (sum3, rest) -> (sum3, rest)
+            | (sum3, rest, chars) -> (sum3, rest, chars)
 
 let rec process_file_impl file prev_numbers prev_chars =
     let line = try input_line file with End_of_file -> "" in
@@ -65,7 +75,7 @@ let rec process_file_impl file prev_numbers prev_chars =
         match parse_line line with
         | (numbers, characters) ->
             match process_data numbers prev_numbers characters prev_chars with
-            | (sum, limited_nums) -> sum + process_file_impl file limited_nums characters
+            | (sum, limited_nums, modified_chars) -> sum + process_file_impl file limited_nums modified_chars
 
 let process_file file =
     process_file_impl file [] []
@@ -75,3 +85,4 @@ let () =
     let res = process_file file in
     Printf.printf "Result: %d\n" res;
     ()
+
