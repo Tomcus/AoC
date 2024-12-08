f45949070daf5bb94f4602f08138fb81aa8a7b4d
Tomas Vybiral <vybiraltomas@seznam.cz>
2024-12-08 20:52:17 +0100

    Solved 2024-03-01

---
+++ 2024/problem_03_01/src/main.rs
@@ -0,0 +1,40 @@
+use anyhow::*;
+use std::io::Read;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+fn solve(input: &str) -> Result<isize> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    }
+    let mut score = 0;
+    for capture in RE.captures_iter(input) {
+        let lhs = isize::from_str_radix(capture.get(1).unwrap().as_str(), 10)?;
+        let rhs = isize::from_str_radix(capture.get(2).unwrap().as_str(), 10)?;
+        //println!("{lhs} * {rhs} = {}", lhs * rhs);
+        score += lhs * rhs;
+    }
+    Ok(score)
+}
+
+fn main() -> Result<()> {
+    let mut buffer = Vec::new();
+    let stdin = std::io::stdin();
+    let mut handle = stdin.lock();
+    handle.read_to_end(&mut buffer)?;
+    let input = String::from_utf8(buffer)?;
+    let res = solve(&input)?;
+    println!("Result: {}", res);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_example() {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let res = solve(&input).unwrap();
+        assert_eq!(res, 161);
+    }
+}
